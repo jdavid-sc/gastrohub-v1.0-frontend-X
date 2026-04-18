4441277370b2ad620de27dcfa074cd15b56d984c
Juan David Saldarriaga <juan.saldarriaga@imexhs.com>
2026-04-18 12:19:33 -0500

    Añadi el ajuste para amtener la sesion una vez se recargue el navegador

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,24 +1,23 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Rutas públicas que se pueden pre-renderizar
   {
     path: 'login',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Prerender
   },
   {
-    path: 'admin/usuarios/editar/:id',
-    renderMode: RenderMode.Client
+    path: 'request-reset',
+    renderMode: RenderMode.Prerender
   },
   {
-    path: 'admin/productos/editar/:id',
-    renderMode: RenderMode.Client
+    path: 'reset-password',
+    renderMode: RenderMode.Prerender
   },
-  {
-    path: 'mesero/pedidos/:id',
-    renderMode: RenderMode.Client
-  },
+  // Todas las demás rutas se renderizan en el cliente
+  // (las rutas protegidas dependen de localStorage/token)
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
